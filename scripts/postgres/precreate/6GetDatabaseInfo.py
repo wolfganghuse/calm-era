bcfa4cd984987a5ea591368f49d767efc5493c65
wolfganghuse <wolfgang.huse@nutanix.com>
2021-01-15 12:57:56 +0100

    Update 6GetDatabaseInfo.py
    updated to v0.9 API

--- scripts/postgres/precreate/6GetDatabaseInfo.py
+++ scripts/postgres/precreate/6GetDatabaseInfo.py
@@ -4,14 +4,14 @@ era_pass = "@@{era_creds.secret}@@"
 headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
 # Get DB Server IP and ID
-url = "https://@@{era_ip}@@:8443/era/v0.8/databases/name/@@{DB_ENTITY_NAME}@@?detailed=true"
+url = "https://@@{era_ip}@@/era/v0.9/databases/@@{DB_ENTITY_NAME}@@?value-type=name&detailed=true&load-dbserver-cluster=true"
 resp = urlreq(
     url, verb="GET", auth="BASIC", user=era_user, passwd=era_pass, headers=headers
 )
+
 if resp.ok:
-    print("DB_SERVER_IP={0}".format(json.loads(resp.content)["hostIP"]))
+    print("DB_SERVER_IP={0}".format(json.loads(resp.content)["databaseNodes"][0]["dbserver"]["ipAddresses"]))
     print("DB_ID={0}".format(json.loads(resp.content)["id"]))
-    print("DB_SERVER_ID={0}".format(json.loads(resp.content)["primaryHost"]))
 else:
     print("Get DB info request failed", json.dumps(json.loads(resp.content), indent=4))
     exit(1)
