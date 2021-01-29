3a82141982a334a9abad83940bc753750a1d989c
wolfganghuse <wolfgang.huse@nutanix.com>
2021-01-29 13:22:04 +0100

    Update erapostgres.py
    added FEAT

--- erapostgres.py
+++ erapostgres.py
@@ -80,7 +80,7 @@ class Era_PostgreSQL_DB(Substrate):
             filename="scripts/postgres/precreate/6GetDatabaseInfo.py",
             variables=["DB_SERVER_IP", "DB_ID", "DB_SERVER_ID"],
         )
-            CalmTask.Exec.escript(
+        CalmTask.Exec.escript(
             name="7newFeat",
             filename="scripts/postgres/postdelete/4MonitorDeregOp.py",
         )
