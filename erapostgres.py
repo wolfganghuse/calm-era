8ff8f90382a2d828a5d06d46ead98f33a7b49781
wolfganghuse <wolfgang.huse@nutanix.com>
2021-02-02 11:45:55 +0100

    Update erapostgres.py
    added postprocess

--- erapostgres.py
+++ erapostgres.py
@@ -104,6 +104,10 @@ class Era_PostgreSQL_DB(Substrate):
             name="4MonitorDeregOp",
             filename="scripts/postgres/postdelete/4MonitorDeregOp.py",
         )
+        CalmTask.Exec.escript(
+            name="5PostProzess",
+            filename="scripts/postgres/postdelete/4MonitorDeregOp.py",
+        )
 
 
 class PostgresDeployment(Deployment):
